a482cd6cf42e6498b3a45e72017a16b1da8a63ab
Thomas Thephasdin <thomas.thephasdin@adesso.de>
2024-10-07 20:30:54 +0200

    add shortening to git_branch.zsh

--- sections/git_branch.zsh
+++ sections/git_branch.zsh
@@ -28,6 +28,23 @@ spaceship_git_branch() {
   git_current_branch="${git_current_branch#heads/}"
   git_current_branch="${git_current_branch/.../}"
 
+  local git_name_length_pre=18
+  local git_name_length_post=8
+  local zero='%([BSUbfksu]|([FK]|){*})'
+  local git_current_branch_length=${#${(S%%)git_current_branch//$~zero/}}
+
+  if (( $git_current_branch_length > ($git_name_length_pre + $git_name_length_post + 1) )); then
+    local git_current_branch_pre="%$git_name_length_pre>…>$(git_current_branch)%>>"
+    # local git_current_branch_pre="%$git_name_length_pre>…>$(git_current_branch)%>>"
+    if (( $git_name_length_post > 0 )); then
+      local git_current_branch_post="…${git_current_branch:(-$git_name_length_post)}"
+      local git_current_branch_post="${git_current_branch:(-$git_name_length_post)}"
+      git_current_branch="$git_current_branch_pre$git_current_branch_post"
+    else
+      git_current_branch="$git_current_branch_pre"
+    fi
+  fi
+
   spaceship::section \
     --color "$SPACESHIP_GIT_BRANCH_COLOR" \
     "$SPACESHIP_GIT_BRANCH_PREFIX$git_current_branch$SPACESHIP_GIT_BRANCH_SUFFIX"
